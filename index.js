0f7fc86b13cfd5f60a0aed83a02903ea94ed9594
Yassin Divin <yassindivin200@gmail.com>
2025-02-10 21:36:26 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,17 +3,12 @@ var codes = document.getElementById("codes");
 var jems = document.getElementById("jems");
 var divin = document.getElementById("divin");
 divin.onclick = ()=> {
-    // result.style.display = "block";
-    // result.style.height = "auto";
-    // result.style.width = "auto";
     result.innerHTML = codes.value;
     localStorage.setItem("Result" , codes.value);
 }
 jems.onclick = ()=> {
-    // result.style.display = "none";
-    result.innerHTML = "";
     localStorage.clear();
 }
 onload = ()=> {
-    codes.value = localStorage.getItem("Result");
+    // codes.value = localStorage.getItem("Result");
 }
